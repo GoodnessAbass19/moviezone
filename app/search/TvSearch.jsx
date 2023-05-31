e0defa8764515ac34a47e2b716c92613c799e6ad
Abass Success <successabass@ymail.com>
2023-05-31 10:54:53 +0100

    fixed the navbar

--- app/search/TvSearch.jsx
+++ app/search/TvSearch.jsx
@@ -27,7 +27,9 @@ const TvSearch = ({ id, page }) => {
   return (
     data && (
       <div className="max-w-full lg:m-20 md:m-15 m-5">
-        <h2 className="text-2xl font-semibold pt-10">Tv-Shows results</h2>
+        <h2 className="text-xl md:text-2xl font-semibold pt-10">
+          Tv-Shows results
+        </h2>
         <div className="grid lg:grid-cols-fluid md:grid-cols-4 grid-cols-2 py-10 md:gap-10 gap-5 mx-auto">
           {data.results.map((item) => (
             <Tv
